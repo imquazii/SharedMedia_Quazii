5ab6d4cb9bd496b6cdfa491a33c3ecd84fbf85e6
Quazii <contact@quazii.gg>
2025-03-12 20:58:54 +0800

    Initial commit

---
+++ Quazii.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "Quazii", [[Interface\Addons\SharedMedia_MyMedia\font\Quazii.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   TEXTURE --
+-- -----
+
+LSM:Register("statusbar", "Quazii", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Quazii.tga]]) 
